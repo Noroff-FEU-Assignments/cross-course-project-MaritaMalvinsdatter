31220a59f99cfa2f701f52aec000152573dad8ae
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2022-03-21 10:52:10 +0100

    Added photos, started making listofjackets array

---
+++ js/jackets.js
@@ -0,0 +1,24 @@
+let listOfJackets = [
+    {
+        name: "Mount Sinai",
+        colors: ["yellow", "blue", "green"],
+        price: "$150",
+        img: "images\shutterstock_490157665.jpg",
+        description: "Light-weight rain jackets for long days out in the rain. Slim fit",
+        size: ["S", "M", "L", "XL", "2XL"],
+        category: "Women's",
+        id: "0001",
+    },
+    {
+        name: "Mount Sinai",
+        colors: ["yellow", "blue", "green"],
+        price: "$150",
+        img: "images\shutterstock_490157800.jpg",
+        description: "Light-weight rain jackets for long days out in the rain. Slim fit",
+        size: ["S", "M", "L", "XL", "2XL"],
+        category: "Women's",
+        id: "0001",
+    }
+]
+
+console.log(listOfJackets.name);
