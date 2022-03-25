c60ccfc675e6e9bbba352a35cc9ac4467bf1fb52
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2022-03-25 09:24:05 +0100

    Jackets info done

--- js/product.js
+++ js/product.js
@@ -15,11 +15,37 @@ function findJacket(product) {
         <h1>${product.name}</h1>
         <img src="${product.img}">
         <div class="jacket-details>
-            <p>${product.category}</p>
+            <div class="flex-price">
+                <p>${product.category}</p>
+                <p>${product.price}</p>
+                <p>Wishlist <i class="far fa-heart"></i></p>
+            </div>
+            <p>Colors:</p>
+            <div class="jacket-colors">
+                <div class="flex-colors">
+                  <span class="circle-red"></span>
+                  <p>Red</p>
+                </div>
+                <div class="flex-colors">
+                  <span class="circle-blue"></span>
+                  <p>Blue</p>
+                </div>
+                <div class="flex-colors">
+                  <span class="circle-yellow"></span>
+                  <p>Yellow</p>
+                </div> 
+            </div>
+            <div  class="size_cta">
+              <label for="sizes">Size:</label>
+              <select name="sizes" id="sizes">
+                <option value="s">S</option>
+                <option value="m">M</option>
+                <option value="l">L</option>
+                <option value="xl">XL</option>
+                <option value="2xl">2XL</option>
+              </select>
         </div>
         </div>`
-
-
         
     } 
 }
